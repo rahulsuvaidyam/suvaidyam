4f0987d8a919bcd52c0333827f623dbc0880867e
rahulsuvaidyam <rahul.suvaidyam@gmail.com>
2023-11-15 12:31:51 +0530

    beneficiary calling popup add

--- suvaidyam/suvaidyam/doctype/beneficiary/beneficiary.js
+++ suvaidyam/suvaidyam/doctype/beneficiary/beneficiary.js
@@ -3,50 +3,51 @@
 
 frappe.ui.form.on("Beneficiary", {
     refresh(frm) {
-        if(frm?.doc?.first_name !== undefined){
-        let d = new frappe.ui.Dialog({
-            title: 'Make a call',
-            fields: [
-                {
-                    label: 'Full Name',
-                    fieldname: 'full_name',
-                    fieldtype: 'Data',
-                    default:frm?.doc?.first_name +' '+frm?.doc?.last_name,
-                    read_only: 1,
-                },
-                {
-                    label: 'Campaign',
-                    fieldname: 'campaign',
-                    fieldtype: 'Autocomplete',
-                    options:frm?.doc?.campaign?.map((e)=>{
-                        return{
-                            label:e?.campaign,
-                            value:e?.name
+        // calling popup
+        if (frm?.doc?.first_name !== undefined) {
+            let d = new frappe.ui.Dialog({
+                title: 'Make a call',
+                fields: [
+                    {
+                        label: 'Full Name',
+                        fieldname: 'full_name',
+                        fieldtype: 'Data',
+                        default: frm?.doc?.first_name + ' ' + frm?.doc?.last_name,
+                        read_only: 1,
+                    },
+                    {
+                        label: 'Campaign',
+                        fieldname: 'campaign',
+                        fieldtype: 'Link',
+                        options: 'Campaign',
+                        get_query: function () {
+                            let filters = (frm?.doc?.campaign || []).map(item => item.campaign);
+                            return { filters: [['name', 'in', filters]] };
                         }
-                    })
+                    },
+                ],
+                size: 'small',
+                secondary_action_label: 'Cancel',
+                primary_action_label: 'Call',
+                primary_action(values) {
+                    if (values?.campaign) {
+                        d.hide();
+                        frappe.show_alert({ message: "Calling to " + values.full_name, indicator: "green" });
+                        // frappe.set_route('List', 'Campaign',{name:values?.campaign});
+                    } else {
+                        frappe.show_alert({ message: "Please select to campaign", indicator: "yellow" });
+                    }
                 },
-            ],
-            size: 'small',  
-            secondary_action_label: 'Cancel',
-            primary_action_label: 'Call',
-            primary_action(values) {
-                if(values?.campaign){
+                secondary_action() {
                     d.hide();
-                    frappe.show_alert({ message:"Calling to " +values.full_name, indicator: "green"});
-                    // frappe.set_route('List', 'Campaign',{name:values?.campaign});
-                }else{
-                    frappe.show_alert({ message:"Please select to campaign", indicator: "yellow"});
                 }
-            },
-            secondary_action(){
-                d.hide();
-            }
-        });
-        
-        frm.add_custom_button('+ Make a call', ()=>{
-             d.show();
-        })
-    }
+            });
+
+            frm.add_custom_button('+ Make a call', () => {
+                d.show();
+            })
+        }
+        // depended dropdown
         frm.fields_dict["centre"].get_query = function (doc) {
             return { filters: { 'state': 'Please select state' } };
         },
@@ -100,29 +101,29 @@ frappe.ui.form.on("Beneficiary", {
                 return { filters: { 'district': 'Please select district' } };
             }
         },
-        frm.set_value('block', '')
-            frm.fields_dict["village"].get_query = function (doc) {
-                if (doc.block) {
-                    return {
-                        filters: { 'block': doc.block },
-                        page_length: 1000
-                    };
-                } else {
-                    return { filters: { 'block': 'Please select block' } };
-                }
-            },
-            frm.set_value('village', '')
-            frm.fields_dict["campaign"].get_query = function (doc) {
-                if (doc.centre) {
-                    return {
-                        filters: { 'centre': doc.centre },
-                        page_length: 1000
-                    };
-                } else {
-                    return { filters: { 'centre': 'Please select centre' } };
-                }
+            frm.set_value('block', '')
+        frm.fields_dict["village"].get_query = function (doc) {
+            if (doc.block) {
+                return {
+                    filters: { 'block': doc.block },
+                    page_length: 1000
+                };
+            } else {
+                return { filters: { 'block': 'Please select block' } };
             }
-            frm.set_value('campaign', '')
+        },
+            frm.set_value('village', '')
+        frm.fields_dict["campaign"].get_query = function (doc) {
+            if (doc.centre) {
+                return {
+                    filters: { 'centre': doc.centre },
+                    page_length: 1000
+                };
+            } else {
+                return { filters: { 'centre': 'Please select centre' } };
+            }
+        }
+        frm.set_value('campaign', '')
     },
     centre: function (frm) {
         frm.set_value('campaign', '')
